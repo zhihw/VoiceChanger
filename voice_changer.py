060f7faa16164ed7e8d1ad644f64fd26448edab4
unknown <wzhlephone@gmail.com>
2024-03-15 23:58:11 -0700

    comment

--- voice_changer.py
+++ voice_changer.py
@@ -9,7 +9,49 @@ from datetime import datetime
 from scipy.io import wavfile
 
 
+def wav_generater(data):
+    #Generate wav file using data
+    try:
+        data = data.astype(np.int16)
+        current_time = datetime.now().strftime("%Y%m%d-%H%M")
+        filename = f"vc_{current_time}.wav"
+        wavfile.write(filename, 44100, data)
+        print(f"File saved as {filename}")
+    except Exception as e:
+        print(f"An error occurred while generating WAV file: {str(e)}")
+
+def read_wav_file(filename):
+    #Read wav file
+    try:
+        sr, data = wavfile.read(filename)
+        data = data.astype(np.int16)
+        return data
+    except Exception as e:
+        print(f"An error occurred while reading WAV file: {str(e)}")
+        return None
+
+def low_pass_filter(signal, sr, cutoff):
+    #Low pass filter
+    try:
+        sos = ss.butter(10, cutoff, fs=sr, btype='low', output='sos')
+        filtered_data = ss.sosfilt(sos, signal)
+        return filtered_data
+    except Exception as e:
+        print(f"An error occurred in low_pass_filter: {str(e)}")
+
+def high_pass_filter(signal, sr, cutoff):
+    #High pass filter
+    try:
+        sos = ss.butter(10, cutoff, fs=sr, btype='high', output='sos')
+        filtered_data = ss.sosfilt(sos, signal)
+        return filtered_data
+    except Exception as e:
+        print(f"An error occurred in high_pass_filter: {str(e)}")
+    
 def record_voice():
+    #This function records the input sound data in mono and at a sampling rate of 44100. 
+    #It reads 1024 frames of data from the audio stream each time until the user presses the Esc key to stop recording.
+    #Then, it merges the data into a NumPy array and returns this array.
     try:
         audio = pyaudio.PyAudio()
         stream = audio.open(format=pyaudio.paInt16,
@@ -36,34 +78,9 @@ def record_voice():
         return audio_data
     except Exception as e:
         print(f"An error occurred while recording: {str(e)}")
-
-def wav_generater(data):
-    try:
-        data = data.astype(np.int16)
-        current_time = datetime.now().strftime("%Y%m%d-%H%M")
-        filename = f"vc_{current_time}.wav"
-        wavfile.write(filename, 44100, data)
-        print(f"File saved as {filename}")
-    except Exception as e:
-        print(f"An error occurred while generating WAV file: {str(e)}")
-
-def low_pass_filter(signal, sr, cutoff):
-    try:
-        sos = ss.butter(10, cutoff, fs=sr, btype='low', output='sos')
-        filtered_signal = ss.sosfilt(sos, signal)
-        return filtered_signal
-    except Exception as e:
-        print(f"An error occurred in low_pass_filter: {str(e)}")
-
-def high_pass_filter(signal, sr, cutoff):
-    try:
-        sos = ss.butter(10, cutoff, fs=sr, btype='high', output='sos')
-        filtered_signal = ss.sosfilt(sos, signal)
-        return filtered_signal
-    except Exception as e:
-        print(f"An error occurred in high_pass_filter: {str(e)}")
-
+        
 def remove_noise(data):
+    #The frequency range from 300 Hz to 3400 Hz is the standard frequency bandwidth for human voice communication. This function will filter out irrelevant sounds based on this range.
     try:
         normalized_data = data / np.max(np.abs(data))
         #Normalize the data
@@ -74,14 +91,19 @@ def remove_noise(data):
         filtered_data2 = low_pass_filter(filtered_data1, 44100, cutoff=3400)
         #Low pass filter to remove high-frequency noise
         
-        restored_data = filtered_data2 * np.max(np.abs(data))
-        return restored_data.astype(np.int16)
+        clear_data = filtered_data2 * np.max(np.abs(data))
+        #denormalization
+
+        return clear_data.astype(np.int16)
     
     except Exception as e:
         print(f"An error occurred while removing noise: {str(e)}")
 
 def analyze_audio(data):
-
+ #The function determines the gender of the voice by analyzing the pitch and spectral centroid, calculating the average pitch of the voiced frames and the average spectral centroid of the audio.
+ #The threshold of 165 for the pitch was established through multiple tests, under the premise of normal speech. 
+ #Pitches lower than 80 or higher than 300 typically occur when speaking intentionally loud or soft. 
+ #In this case，to accurately determine the gender, manual confirmation from the user is required.
     try:
         data = data.astype(float)
         sr = 44100 
@@ -127,11 +149,15 @@ def analyze_audio(data):
 
 
 def change_gender(data, sr=44100):
+    #This function uses pitch shifting and time stretching techniques to simulate the voice characteristics of another gender, in addition to using filters to enhance the effect. 
+    #The number of steps for pitch shifting has a base value, which is then fine-tuned dynamically based on the target pitch and the current pitch. 
+    #The time stretch ratio, however, is fixed.
+    #In special cases where the pitch falls below 80 or above 300, to prevent excessive pitch correction, the number of steps is adjusted to a fixed value.
     try:
         low_pitch_thres=80
-        high_pitch_thres=250
-        male_target_pitch = 115
-        female_target_pitch = 170
+        high_pitch_thres=300
+        male_target_pitch = 130
+        female_target_pitch = 240
         gender, avg_pitch = analyze_audio(data)
 
         if gender is None or avg_pitch == 0:
@@ -141,20 +167,20 @@ def change_gender(data, sr=44100):
             
         if gender == 'Male':
             if avg_pitch < low_pitch_thres or avg_pitch > high_pitch_thres:
-                n_steps = 8
+                n_steps = 8 #special cases for male is 8
             else: 
-                base_n_steps = 5  
-                pitch_diff = avg_pitch - male_target_pitch  
-                n_steps_adjustment = pitch_diff / 100  
+                base_n_steps = 5 #base value for male is 5
+                pitch_diff = female_target_pitch - avg_pitch
+                n_steps_adjustment = pitch_diff / 50 
                 n_steps = base_n_steps + n_steps_adjustment
             s_rate = 0.9
         elif gender == 'Female':
             if avg_pitch < low_pitch_thres or avg_pitch > high_pitch_thres:
-                n_steps = -8
+                n_steps = -8 #special cases for female is -8
             else:
-                base_n_steps = -5  
+                base_n_steps = -3  #base value for female is -3
                 pitch_diff = avg_pitch - male_target_pitch  
-                n_steps_adjustment = pitch_diff / 100  
+                n_steps_adjustment = pitch_diff / 50
                 n_steps = base_n_steps - n_steps_adjustment
             s_rate = 1.1
 
@@ -165,16 +191,18 @@ def change_gender(data, sr=44100):
         #Stretch time
         if gender == 'Male':
             data_stretched = high_pass_filter(data_stretched, sr, cutoff=500)
+        #Use a high-pass filter to filter low-frequency sounds and further emphasize high-frequency sounds
         elif gender == 'Female':
-            data_stretched = low_pass_filter(data_stretched, sr, cutoff=1500) 
+            data_stretched = low_pass_filter(data_stretched, sr, cutoff=2000) 
         return data_stretched.astype(np.int16),n_steps
-
+        #Use a low-pass filter to filter high-frequency sounds and further emphasize low-frequency sounds
     except Exception as e:
         print(f"An error occurred while changing voice gender: {str(e)}")
         raise e
 
 
 def play_audio(data, sr=44100):
+    #Use sounddevice play and wait to play sounds and simulate the effect of the progress bar.
     try:
         duration = len(data) / sr  
         sd.play(data, sr)  
@@ -190,17 +218,21 @@ def play_audio(data, sr=44100):
             progress_bar_length = 50  
             filled_length = int(progress_bar_length * progress)
             bar = '█' * filled_length + '-' * (progress_bar_length - filled_length)
-            print(f'\rAudio Playback: |{bar}| {progress * 100:.2f}%', end='\r')
+            print(f'\rPlay Audio: |{bar}| {progress * 100:.2f}%', end='\r')
             time.sleep(0.1)  
         bar = '█' * progress_bar_length
-        print(f'\rAudio Playback: |{bar}| 100.00%', end='\r')
+        print(f'\rPlay Audio: |{bar}| 100.00%', end='\r')
         sd.wait()  
-        print('\nAudio playback completed.')
+        print('\ncompleted.')
     except Exception as e:
         print(f"An error occurred while playing audio: {str(e)}")
 
+
+
 while True:
-    data = record_voice()
+    data=read_wav_file("lmed.wav")
+    #data=record_voice()
+    #wav_generater(data)
     if data is not None:
         data = remove_noise(data)
         analysis_result = analyze_audio(data)
@@ -210,6 +242,7 @@ while True:
             print(analysis_result)
             data,n = change_gender(data)
             print(n)
-            play_audio(data)
-            wav_generater(data)
+            while(1):
+                play_audio(data)
+                #wav_generater(data)
             break
